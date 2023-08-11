a7ce665c7e441f1656b5dfc9d1fe280bd95bb421
Hassan Moussa <hassanmoussa61@gmail.com>
2023-08-12 00:00:14 +0300

    added child details page

--- front_end/src/ParentWork/pages/Child_details.jsx
+++ front_end/src/ParentWork/pages/Child_details.jsx
@@ -7,9 +7,35 @@ function ChildPage() {
   const child = location.state || {};
 
   return (
-    <div>
-      <h2>{child.name || "Child Name"}</h2>
-      {/* Display child's progress, grades, assignments, etc. */}
+    <div className="child-details-container">
+      <h2 className="child-name">{child.name || "Child Name"}</h2>
+      <div className="child-progress">
+        <h3>Progress</h3>
+        <p>{child.progress || 0}%</p>
+      </div>
+      <div className="child-grades">
+        <h3>Grades</h3>
+        <ul>
+          {child.grades &&
+            child.grades.map((grade, index) => (
+              <li key={index}>
+                {grade.course}: {grade.grade}
+              </li>
+            ))}
+        </ul>
+      </div>
+      <div className="child-assignments">
+        <h3>Assignments</h3>
+        <ul>
+          {child.assignments &&
+            child.assignments.map((assignment, index) => (
+              <li key={index}>
+                {assignment.title} - Due: {assignment.dueDate} - Status:{" "}
+                {assignment.status}
+              </li>
+            ))}
+        </ul>
+      </div>
     </div>
   );
 }
