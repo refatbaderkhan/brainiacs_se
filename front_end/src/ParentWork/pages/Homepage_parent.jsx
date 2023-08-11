525846c622931cb961fa2b8dd89111b54b60b823
Hassan Moussa <hassanmoussa61@gmail.com>
2023-08-11 17:24:41 +0300

    added some cards with styling

--- front_end/src/ParentWork/pages/Homepage_parent.jsx
+++ front_end/src/ParentWork/pages/Homepage_parent.jsx
@@ -2,7 +2,36 @@ import React from "react";
 import "./Homepage_parent.css";
 
 function Homepage_parent() {
-  return <div></div>;
+  return (
+    <div className="parent-container">
+      <div className="greeting_parent">
+        <div className="parent-image"></div>
+        <h1>{`Hello Parent !`}</h1>
+      </div>
+
+      <div className="choose-section">
+        <h2>What DO You Want To Do ?</h2>
+
+        <div className="card-selection">
+          <div className="card-select">
+            <p>
+              Progress <br />
+              Monitoring
+            </p>
+          </div>
+          <div className="card-select">
+            <p>Communication with Teachers</p>
+          </div>
+          <div className="card-select">
+            <p>Attendance and Schedule</p>
+          </div>
+          <div className="card-select">
+            <p>Notifications and Reminders</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Homepage_parent;
