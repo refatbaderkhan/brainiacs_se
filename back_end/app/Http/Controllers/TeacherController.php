fed15a9c2d68f2bf3cf04232e3f14a1ed97a773d
refatbaderkhan <refatbaderkhan@gmail.com>
2023-08-13 17:42:58 +0300

    added createAssignment function

--- back_end/app/Http/Controllers/TeacherController.php
+++ back_end/app/Http/Controllers/TeacherController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\CourseMaterial;
+use App\Models\Assignment;
 
 
 class TeacherController extends Controller
@@ -28,8 +29,35 @@ class TeacherController extends Controller
 
       return response()->json([
           'status' => 'Success',
-          'message' => 'Course material created successfully',
+          'message' => 'Course Material created successfully',
           'data' => $CourseMaterial,
+      ]);
+  }
+
+  public function createAssignment(Request $request)
+  {
+      $request->validate([
+          'course_id' => 'required|exists:courses,id',
+          'title' => 'required|string|max:255',
+          'description' => 'required|string',
+          'content_url' => 'required|string',
+          'due_date' => 'nullable|date_format:Y-m-d H:i:s',
+      ]);
+
+      $Assignment = new Assignment([
+          'course_id' => $request->course_id,
+          'title' => $request->title,
+          'description' => $request->description,
+          'content_url' => $request->content_url,
+          'due_date' => $request->due_date,
+      ]);
+
+      $Assignment->save();
+
+      return response()->json([
+          'status' => 'Success',
+          'message' => 'Assignment created successfully',
+          'data' => $Assignment,
       ]);
   }
 }
--- back_end/routes/api.php
+++ back_end/routes/api.php
@@ -30,6 +30,7 @@ Route::group(["middleware" => "auth:api"], function(){
         Route::group(["prefix" => "teacher"], function(){
           Route::get("trust_issues", [AuthController::class, "issues"]);
           Route::post("create_material", [TeacherController::class, "createMaterial"]);
+          Route::post("create_assignment", [TeacherController::class, "createAssignment"]);
         });
     });
 
