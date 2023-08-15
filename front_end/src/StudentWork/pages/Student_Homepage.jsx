36b8a62dd9fd7651e6bdf357659e3de83430a51f
Hassan Moussa <hassanmoussa61@gmail.com>
2023-08-16 01:08:24 +0300

    added the ability to enroll in course

--- front_end/src/StudentWork/pages/Student_Homepage.jsx
+++ front_end/src/StudentWork/pages/Student_Homepage.jsx
@@ -41,24 +41,38 @@ function StudentHomePage() {
 
   const enrollInCourse = (courseId) => {
     axios
-      .post(
-        `http://127.0.0.1:8000/api/student/enroll-in-course/${courseId}`,
-        null,
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        }
-      )
-      .then((response) => {
-        // Handle enrollment success
-        // You can fetch enrolled courses again to update the list
+      .post(`http://127.0.0.1:8000/api/enroll/${courseId}`, null, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then(() => {
+        // Fetch updated enrolled courses list
+        axios
+          .get("http://127.0.0.1:8000/api/enrolled-courses", {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          })
+          .then((response) => {
+            setEnrolledCourses(response.data.enrolled_courses);
+
+            const enrolledCourse = courses.find(
+              (course) => course.id === courseId
+            );
+
+            setCourses((prevCourses) =>
+              prevCourses.filter((course) => course.id !== courseId)
+            );
+          })
+          .catch((error) => {
+            console.error("Error fetching enrolled courses:", error);
+          });
       })
       .catch((error) => {
         console.error("Error enrolling in course:", error);
       });
   };
-
   const selectCourse = (course) => {
     setSelectedCourse(course);
   };
@@ -69,15 +83,13 @@ function StudentHomePage() {
 
       <h2>Enrolled Courses:</h2>
       <div className="course-cards">
-        {enrolledCourses.map((course) => (
-          <div className="course-card" key={course.id}>
-            <h3>{course.course.title}</h3>
-
-            <button onClick={() => selectCourse(course)}>Access</button>
+        {enrolledCourses.map((enrolledCourse) => (
+          <div className="course-card" key={enrolledCourse.id}>
+            <h3>{enrolledCourse.course.title}</h3>
+            <button onClick={() => selectCourse(enrolledCourse)}>Access</button>
           </div>
         ))}
       </div>
-
       <h2>Available Courses:</h2>
       <div className="course-cards">
         {courses.map((course) => (
